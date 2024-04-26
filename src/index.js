4cbe1fc2e14dcf00e9a54d79378f637fb1c12f64
Garrett <gsharp.impact@gmail.com>
2024-04-26 10:42:58 -0500

    save

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 export { default as Component } from './Component.svelte';
+export { default as TimeEntryCard } from './TimeEntryCard.svelte';
+export { default as ComponentBase } from './ComponentBase.svelte';
 
 // import Component from './Component.svelte';
